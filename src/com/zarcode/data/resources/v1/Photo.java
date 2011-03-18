38945528c10525db6e38922356a49420e1d02903
Lazar Kirven <lrkirven@gmail.com>
2011-03-18 14:54:56 -0500

    minor fix

--- src/com/zarcode/data/resources/v1/Photo.java
+++ src/com/zarcode/data/resources/v1/Photo.java
@@ -50,7 +50,7 @@ public class Photo extends ResourceBase {
 		bucket.setAlbumId(album.getId());
 		bucket.setBytesUsed(album.getBytesUsed());
 		bucket.setFullFlag(false);
-		bucket.setAlbumName(album.getName());
+		bucket.setAlbumName(album.getTitle().getPlainText());
 		bucket.setRemainingPhotos(album.getPhotosLeft());
 		return bucket;
 	}
